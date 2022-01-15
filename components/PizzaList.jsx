928f91b21062b456c7f3fa40169868cf95921a82
luckyman1004 <luckysuperdev@gmail.com>
2022-01-15 19:32:41 +0800

    Initial Commit

---
+++ components/PizzaList.jsx
@@ -0,0 +1,24 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard"
+
+const PizzaList = ({pizzaList}) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{pizzaList.title}</h1>
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit arcu
+        in pretium molestie. Interdum et malesuada fames acme. Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit.
+      </p>
+      <div className={styles.wrapper}>
+      { pizzaList.map((pizza) => (
+        <PizzaCard key={pizza._id} pizza={pizza}/>
+         
+      ))}
+      
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
---
+++ pages/api/products/index.js
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import dbConnect from '../../../util/mongo'
+import Product from '../../../models/Product'
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  dbConnect()
+
+  if (method === 'GET') {
+      try {
+
+        const products = await Product.find();
+        res.status(200).json(products);
+      } catch(err) {
+          res.status(500).json(err)
+      }
+  }
+
+  if (method === 'POST') {
+      try {
+
+        const product = await Product.create(req.body);
+        res.status(201).json(product)
+      } catch(err) {
+          res.status(500).json(err);
+      }
+  }
+}
+  
