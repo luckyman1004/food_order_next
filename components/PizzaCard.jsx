2b89dc50099f831b330de3940eabb0a17f75b06d
luckyman1004 <luckysuperdev@gmail.com>
2022-01-16 12:36:31 +0800

    Remove data-testid

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -6,7 +6,7 @@ const PizzaCard = ({pizza}) => {
   return (
     <div className={styles.container}>
     <Link href={`/product/${pizza._id}`} passHref>
-      <Image data-testid={pizza.description} src={pizza.img} alt="" width="500" height="500" />
+      <Image src={pizza.img} alt="" width="500" height="500" />
       </Link>
       <h1 className={styles.title}>{pizza.title}</h1>
       <span className={styles.price}>${pizza.prices[0]}</span>
