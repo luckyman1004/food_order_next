a27b5a6658e36f9a66ba94437c26bf1144f6c1de
luckyman1004 <luckysuperdev@gmail.com>
2022-01-16 14:04:09 +0800

    Add single item

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,17 +1,11 @@
 import styles from "../../styles/Product.module.css";
 import Image from "next/image";
 import { useState } from "react";
+import axios from "axios";
 
-const Product = () => {
+const Product = ({pizza}) => {
   const [size, setSize] = useState(0);
-  const pizza = {
-    id: 1,
-    img: "/img/pizza.png",
-    name: "CAMPAGNOLA",
-    price: [19.9, 23.9, 27.9],
-    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis arcu purus, rhoncus fringilla vestibulum vel, dignissim vel ante. Nulla facilisi. Nullam a urna sit amet tellus pellentesque egestas in in ante.",
-  };
-
+ 
   return (
     <div className={styles.container}>
       <div className={styles.left}>
@@ -21,7 +15,7 @@ const Product = () => {
       </div>
       <div className={styles.right}>
         <h1 className={styles.title}>{pizza.name}</h1>
-        <span className={styles.price}>${pizza.price[size]}</span>
+        <span className={styles.price}>${pizza.prices[size]}</span>
         <p className={styles.desc}>{pizza.desc}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
@@ -85,5 +79,15 @@ const Product = () => {
     </div>
   );
 };
+
+export const getServerSideProps = async ({params}) => {
+  const res = await axios.get(`http://localhost:3000/api/products/${params.id}`)
+  return {
+    props: {
+      pizza: res.data,
+    }
+  }
+}
+
 
 export default Product;
