a5ac54352e98f217bdde2509b5bc6213cc2800bc
luckyman1004 <luckysuperdev@gmail.com>
2022-01-22 15:08:19 +0800

    Prefix with NEXT_PUBLIC_

--- util/mongo.js
+++ util/mongo.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
-const MONGO_URL = process.env.MONGO_URL
+const NEXT_PUBLIC_MONGO_URL = process.env.NEXT_PUBLIC_MONGO_URL
 
-if (!MONGO_URL) {
+if (!NEXT_PUBLIC_MONGO_URL) {
    
   throw new Error(
     'Please define the MONGO_URL environment variable inside .env.local'
@@ -30,7 +30,7 @@ async function dbConnect() {
       bufferCommands: false,
     }
 
-    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(NEXT_PUBLIC_MONGO_URL, opts).then((mongoose) => {
       return mongoose
     })
   }
