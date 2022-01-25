56ed291b0792b8cedabd5c596e12b8f8ea7f4ec4
luckyman1004 <luckysuperdev@gmail.com>
2022-01-25 10:35:37 +0800

    Env variable issue

--- util/mongo.js
+++ util/mongo.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
-const NEXT_PUBLIC_MONGO_URL = process.env.NEXT_PUBLIC_MONGO_URL
+const MONGO_URL = process.env.MONGO_URL
 
-if (!NEXT_PUBLIC_MONGO_URL) {
+if (!MONGO_URL) {
    
   throw new Error(
     'Please define the MONGO_URL environment variable inside .env.local'
@@ -30,7 +30,7 @@ async function dbConnect() {
       bufferCommands: false,
     }
 
-    cached.promise = mongoose.connect(NEXT_PUBLIC_MONGO_URL, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
       return mongoose
     })
   }
